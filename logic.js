0e6b7da63f2dd0c878427e03bcdac84c00dfd626
Joachim Goennheimer <Joachim.goennheimer@ibm.com>
2019-07-29 18:48:58 +0200

    events can be displayed in monthView

--- logic.js
+++ logic.js
@@ -29,11 +29,23 @@ months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", 
 // Necessary for next, previous buttons to work properly
 inMonthView = true;
 
+var eventData;
+
 $(document).ready(function(){
 
     
     displayMonthView(currentMonth, currentYear);
     displayYearMonthDate(currentDate, currentMonth, currentYear);
+
+    loadEventData().then(function(message){
+
+        displayEventsMonthView();
+        console.log(message);
+        console.log(eventData);
+    })
+    .catch(function(message){
+        console.log(message);
+    })
 
     createNewEntryButton.click(function(){
         console.log("toggling sidebar");
@@ -73,6 +85,19 @@ $(document).ready(function(){
     })
 })
 
+function loadEventData(){
+
+    return new Promise(function(resolve, reject){
+
+        $.get("https://dhbw.cheekbyte.de/calendar/500/events", function(data){
+        eventData = data;
+        resolve("success");
+        reject("loading error");
+        })
+    });
+
+}
+
 
 function displayYearMonthDate(date, month, year){
     dateDisplay.text(date);
@@ -97,17 +122,23 @@ function displayMonthView(month, year){
 
         for(j = 0; j < 7; j++){
 
-            column = "<td>";
 
             // empty cell
             if(i === 0 && j < startDay){
-                column = column + "Not a day";
+                column = "<td>";
 
             }
             // normal entries
             else if(dateCounter <= daysInMonth){
+                // adding id to column
+                var columnID = calculateIDMonthView(year, month, dateCounter);
+                // console.log("columnID: " + columnID);
+                column = "<td id='" + columnID + "'>";
                 column = column + "<h1>" + dateCounter + "</h1>";
                 dateCounter++;
+            }
+            else{
+                column = "<td>";
             }
 
             column = column + "</td>";
@@ -123,30 +154,110 @@ function displayMonthView(month, year){
 
 }
 
+function displayEventsMonthView(){
+
+    var eventStartString;
+
+    var eventYear;
+    var eventMonth;
+    var eventDate;
+
+    var currentYearString = "" + currentYear;
+    var currentMonthString = "" + currentMonth;
+    var currentDayString = "" + currentDate;
+
+    var eventDateID;
+    var eventDIV;
+
+    eventData.forEach(function(event){
+
+        eventStartString = event.start;
+
+        eventYear = eventStartString.slice(0, 4);
+        eventMonth = eventStartString.slice(5, 7);
+
+        // because currentMonth is only one digit if below 10
+        if(eventMonth.slice(0, 1) === "0"){
+            eventMonth = "" + eventMonth.slice(1);
+        }
+
+        eventDate = eventStartString.slice(8, 10);
+
+        eventYear = parseInt(eventYear);
+        eventMonth = parseInt(eventMonth);
+        eventDate = parseInt(eventDate);
+
+        if(eventYear === currentYear && eventMonth === currentMonth){
+
+            console.log(event.title);
+            eventDateID = calculateIDMonthView(eventYear, eventMonth, eventDate);
+            eventDIV = generateEventDIV(event);
+            console.log("eventDateID: " + eventDateID);
+            var eventDateCell = $("#" + eventDateID);
+            eventDateCell.append(eventDIV);
+        }
+
+        // console.log("*******************************");
+        // console.log(event.title);
+        // console.log("event year" + eventYear)
+        // console.log("event month" + eventMonth)
+        // console.log("event day" + eventDate)
+        // console.log(event.start);
+        // console.log(event.end);
+
+    })
+
+}
+
+
 function nextMonth(){
     var daysInMonth = calculateDaysInMonth(currentMonth, currentYear);
+    // calculates the date of the next saturday
     var lastDateOfWeek = currentDate - currentDay + 6;
-    var daysLeftInMonth = daysInMonth - lastDateOfWeek;
+    // console.log("*******************************************");
+    // console.log("currentDate: " + currentDate);
+    // console.log("currentDay: " + currentDay);
+    // console.log("lastDateOfWeek: " + lastDateOfWeek);
+
+    // if the last saturday has a higher date than possible the lastDateOfWeek is not on a saturday which means that the month ends prior in the week
+    if(lastDateOfWeek > daysInMonth){
+        lastDateOfWeek = daysInMonth;
+        // console.log("IN IF - lastDateOfWeek: " + lastDateOfWeek);
+    }
+    // calculates the days that are left in the month
+    var daysLeftInMonth = daysInMonth - currentDate;
+    // console.log("daysLeftInMonth: " + daysLeftInMonth);
     if(currentMonth === 11){
         currentYear++;
         currentMonth = 0;
-        currentDate = 7 - daysLeftInMonth;
-        console.log("currentDate: " + currentDate);
+        currentDate = 7 - (daysLeftInMonth % 7);
+        // console.log("currentDate: " + currentDate);
     }
     else{
         currentMonth++;
+        // calculates the new current date displayed. It will always be the same day of the week as in the month before.
         currentDate = 7 - (daysLeftInMonth % 7);
-        console.log("currentDate: " + currentDate);
+        // console.log("currentDate: " + currentDate);
     }
     displayYearMonthDate(currentDate, currentMonth, currentYear);
     displayMonthView(currentMonth, currentYear);
+    displayEventsMonthView();
 }
 
 function nextWeek(){
     
     var daysInMonth = calculateDaysInMonth(currentMonth, currentYear);
+    // console.log("*******************************************");
+    // console.log("daysInMonth: " + daysInMonth);
     var lastDateOfWeek = currentDate - currentDay + 6;
-    var daysLeftInMonth = daysInMonth - lastDateOfWeek;
+    if(lastDateOfWeek > daysInMonth){
+        lastDateOfWeek = daysInMonth;
+    }
+    // console.log("lastDateOfWeek: " + lastDateOfWeek);
+
+    var daysLeftInMonth = daysInMonth - currentDate;
+    // console.log("daysLeftInMonth: " + daysLeftInMonth);
+
     if(daysLeftInMonth < 7){
         if(currentMonth === 11){
             currentYear++;
@@ -156,6 +267,7 @@ function nextWeek(){
             currentMonth++;
         }
         currentDate = 7 - daysLeftInMonth;
+        // console.log("Current Date: " + currentDate);
     }
     else{
         currentDate += 7;
@@ -165,9 +277,9 @@ function nextWeek(){
 }
 
 function previousWeek(){
-    daysInPrevMonth = calculateDaysPrevMonth(currentMonth, currentYear)
-    var daysInPrevMonth;
-    if(currentDate < 7){
+    var daysInPrevMonth = calculateDaysPrevMonth(currentMonth, currentYear)
+    // var daysInPrevMonth;
+    if(currentDate <= 7){
         if(currentMonth === 0){
             currentYear--;
             currentMonth = 11;
@@ -187,6 +299,8 @@ function previousWeek(){
 
 function previousMonth(){
     daysInPrevMonth = calculateDaysPrevMonth(currentMonth, currentYear)
+    // console.log("#############################################");
+    // console.log("daysInPrevMonth: " + daysInPrevMonth);
     if(currentMonth === 0){
         currentYear--;
         currentMonth = 11;
@@ -194,9 +308,12 @@ function previousMonth(){
     else{
         currentMonth--;
     }
+    // console.log("currenMonth: " + currentMonth);
     currentDate = daysInPrevMonth - ( 7 - (currentDate % 7) );
+    // console.log("currentDate: " + currentDate);
     displayYearMonthDate(currentDate, currentMonth, currentYear);
     displayMonthView(currentMonth, currentYear);
+    displayEventsMonthView();
 }
 
 
@@ -207,11 +324,11 @@ function calculateDaysInMonth(month, year){
 
 function calculateDaysPrevMonth(month, year){
 
-    if(currentMonth === 1){
+    if(currentMonth === 0){
         return calculateDaysInMonth(11, year-1);
     }
     else{
-        return calculateDaysInMonth(month, year);
+        return calculateDaysInMonth(month-1, year);
     }
 
 }
@@ -262,10 +379,11 @@ function displayWeekView(minutes, hour, day, date, month, year){
             timeTracker.setMinutes(30);
         }
         
-
-
         for(j = 0; j < 7; j++){
-            var column = "<td>";
+
+            var columnID = calculateIDWeekView(j, day, date, hoursString, minutesString);
+            // console.log("dateID: " + columnID);
+            var column = "<td>" + columnID;
 
 
             column = column + "</td>";
@@ -284,17 +402,24 @@ function displayWeekView(minutes, hour, day, date, month, year){
 function displayDateSpans(day, date){
 
     var daysinPrevMonth = calculateDaysPrevMonth(currentMonth, currentYear);
+    var daysInCurrentMonth = calculateDaysInMonth(currentMonth, currentYear);
     // console.log(date-day);
     var weekDates = [];
 
-    lastMonthDateCounter = date-day;
-    while(lastMonthDateCounter < 1){
-        weekDates.push(daysinPrevMonth + lastMonthDateCounter);
-        lastMonthDateCounter++;
+    dateNamingCounter = date-day;
+    // displays dates that were part of the last month
+    while(dateNamingCounter < 1){
+        weekDates.push(daysinPrevMonth + dateNamingCounter);
+        dateNamingCounter++;
     }
+    // displays dates that are part of the current month
     while(weekDates.length < 7){
-        weekDates.push(lastMonthDateCounter);
-        lastMonthDateCounter++;
+        // checks whether need to display dates from next month already
+        if(dateNamingCounter > daysInCurrentMonth){
+            dateNamingCounter = 1;
+        }
+        weekDates.push(dateNamingCounter);
+        dateNamingCounter++;
     }
 
     sundayDateSpan.text(weekDates[0]);
@@ -306,6 +431,39 @@ function displayDateSpans(day, date){
     saturdayDateSpan.text(weekDates[6]);
 }
 
+function calculateIDMonthView(year, month, date){
+    return "" + year + "-" + month + "-" + date;
+
+}
+
+function calculateIDWeekView(dayToCalculate, day, date, hour, minutes){
+
+    var daysinPrevMonth = calculateDaysPrevMonth(currentMonth, currentYear);
+    var daysInCurrentMonth = calculateDaysInMonth(currentMonth, currentYear);
+    // console.log(date-day);
+    var weekDates = [];
+
+    dateNamingCounter = date-day;
+    // displays dates that were part of the last month
+    while(dateNamingCounter < 1){
+        weekDates.push(daysinPrevMonth + dateNamingCounter);
+        dateNamingCounter++;
+    }
+    // displays dates that are part of the current month
+    while(weekDates.length < 7){
+        // checks whether need to display dates from next month already
+        if(dateNamingCounter > daysInCurrentMonth){
+            dateNamingCounter = 1;
+        }
+        weekDates.push(dateNamingCounter);
+        dateNamingCounter++;
+    }
+
+    return "" + currentYear + "-" + currentMonth + "-" + weekDates[dayToCalculate] + "T" + hour + minutes;
+
+
+}
+
 function hideDateSpans(){
     sundayDateSpan.text("");
     mondayDateSpan.text("");
@@ -314,4 +472,18 @@ function hideDateSpans(){
     thursdayDateSpan.text("");
     fridayDateSpan.text("");
     saturdayDateSpan.text("");
+}
+
+function generateEventDIV(event){
+    var eventID = event.id;
+    var eventTitle = event.title;
+    var eventStatus = event.status;
+
+    var htmlString = '<div id="' + event.id + '"';
+    htmlString += '<h3>' + eventTitle + '</h3>';
+
+    htmlString += '</div>';
+
+    return htmlString;
+
 }
